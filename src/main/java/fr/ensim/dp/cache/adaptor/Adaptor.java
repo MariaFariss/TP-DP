2f504eebf92dad2210e780cc60366dd9c8c0c533
MariaFariss <maria.fariss.etu@univ-lemans.fr>
2023-03-05 22:20:28 +0100

    commit

---
+++ src/main/java/fr/ensim/dp/cache/adaptor/Adaptor.java
@@ -0,0 +1,37 @@
+package fr.ensim.dp.cache.adaptor;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import javax.imageio.ImageIO;
+
+import org.jdesktop.swingx.mapviewer.TileCache;
+
+import fr.ensim.dp.cache.ICache;
+
+public class Adaptor extends TileCache {
+
+    ICache cache ;
+    @Override
+    public BufferedImage get(URI arg0) throws IOException {
+        byte[] bytes = cache.retreive(arg0.toString());
+        InputStream is = new ByteArrayInputStream(bytes);
+        BufferedImage newBi = ImageIO.read(is);
+
+        return newBi;
+    }
+
+    @Override
+    public void needMoreMemory() {
+        cache.clear();
+    }
+
+    @Override
+    public void put(URI arg0, byte[] arg1, BufferedImage arg2) {
+        cache.add(arg0.toString(), arg1);
+    }
+    
+}
